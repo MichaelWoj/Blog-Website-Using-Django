61ffd3a9929d4a7ab61a6fd44d52cf18aea5f204
Michal <wojnowskimichael@gmail.com>
2019-02-06 14:49:16 +0000

    Tutorial 5

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'widget_tweaks',
+
+    'accounts',
     'boards',
 ]
 
@@ -128,3 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+LOGIN_URL = 'login'
